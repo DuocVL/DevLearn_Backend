1f9a50dc24f1b0b84250c72b78964371de4e0ad9
leduochhnd <leduochhnd@gmail.com>
2026-01-12 09:34:50 +0000

    update get profile

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getMyProfile } = require('../controllers/userController');
+const verifyJWT = require('../middleware/verifyJWT'); // Import the middleware
 
 // @route   GET /users/profile
 // @desc    Get current user's profile
 // @access  Private
-router.get('/profile', getMyProfile);
+router.get('/profile', verifyJWT, getMyProfile); // Apply middleware here
 
 module.exports = router;
