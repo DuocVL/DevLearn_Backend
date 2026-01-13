a31c09e4e272a8cbffc3154981dae68666380826
leduochhnd <leduochhnd@gmail.com>
2026-01-13 19:00:57 +0000

    Update cham code

--- config/languageConfig.js
+++ config/languageConfig.js
@@ -2,16 +2,19 @@ const languageConfig = {
   python: {
     image: 'python:3.9-slim',
     srcFileName: 'main.py',
+    containerDir: '/usr/src/app', // Added
     runCmd: { cmd: 'python3', args: ['main.py'] },
   },
   javascript: {
     image: 'node:18-slim',
     srcFileName: 'main.js',
+    containerDir: '/usr/src/app', // Added
     runCmd: { cmd: 'node', args: ['main.js'] },
   },
   cpp: {
     image: 'gcc:11',
     srcFileName: 'main.cpp',
+    containerDir: '/usr/src/app', // Added
     compileCmd: { cmd: 'g++', args: ['-O2', '-std=c++17', 'main.cpp', '-o', 'a.out'] },
     runCmd: { cmd: './a.out', args: [] },
   },
@@ -19,6 +22,7 @@ const languageConfig = {
   // java: {
   //   image: 'openjdk:11-jre-slim',
   //   srcFileName: 'Main.java',
+  //   containerDir: '/usr/src/app',
   //   compileCmd: { cmd: 'javac', args: ['Main.java'] },
   //   runCmd: { cmd: 'java', args: ['Main'] },
   // }
